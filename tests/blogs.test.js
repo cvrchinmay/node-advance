a9a0532f0dd13f98fa45bec11596d4c988873fd7
chinmayraut <chinmay@clevertap.com>
2020-06-06 17:51:00 +0530

    Refactored get and post

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -65,36 +65,15 @@ describe('When logged in', () => {
 
 describe('User is not logged in', () => {
     test('User cannot create blog posts', async () => {
-        const result = await page.evaluate(
-            () => {
-                return fetch('/api/blogs', {
-                    method: 'POST',
-                    credentials: 'same-origin',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        title: 'Test Title',
-                        content: 'Test Content'
-                    })
-                }).then(res => res.json());
-            }
-        );
+        const result = await page.post('/api/blogs', {
+            title: 'Test Title',
+            content: 'Test Content'
+        })
         expect(result.error).toEqual('You must log in!')
     })
 
     test('User cannot get a list of posts', async () => {
-        const result = await page.evaluate(
-            () => {
-                return fetch('/api/blogs', {
-                    method: 'GET',
-                    credentials: 'same-origin',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                }).then(res => res.json());
-            }
-        );
+        const result = await page.get('/api/blogs');
         expect(result.error).toEqual('You must log in!')
     })
 })
